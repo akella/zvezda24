a31ef1a4692ae4c1166cb52f945e79ddb218a19f
Artur <morozarturv@gmail.com>
2012-12-21 14:39:17 +0200

    done

--- js/common.js
+++ js/common.js
@@ -17,17 +17,19 @@ $(window).scroll(function() {
 });
 
 //filter
-// $('.filter a').click(function() {
-// 	$('.filter a').removeClass('active');
-// 	$(this).addClass('active');
-// 	return false;
-// });
+$('.filter a').click(function() {
+  $('.catalog').hide();
+	var catalog = $(this).attr('href');
+	$('.'+catalog).fadeIn(300);  
+	return false;
+});
+//
 $('.filter').each(function() {
-  var method = 'linear',
+  var method = 'easeInExpo',
       topMenu = $('.filter'),
       activeMenu = $('.active',topMenu),
-      mainlevel = $('li',topMenu),
-      animMenu = $('.filter__die')
+      mainlevel = $('a',topMenu),
+      animMenu = $('.filter__die');
   
   var animFunc = function(el) {
   	var menuWidth = el.outerWidth();
@@ -35,7 +37,7 @@ $('.filter').each(function() {
   	animMenu.stop(true).animate({
   	  left: menuLeft,
   	  width: menuWidth
-  	}, 500, method);
+  	}, 300, method);
   }  
   // если есть активный пункт меню, то позиционируем двигающуюся плашку на нем
   animFunc(activeMenu);
@@ -43,7 +45,11 @@ $('.filter').each(function() {
   mainlevel.click(function() {
     mainlevel.removeClass('active');    
     animFunc($(this));
-    $(this).addClass('active');
+    $(this).delay(300).queue(function(next){
+      $(this).addClass("active");
+      next();
+    });
+    return false;
   })    
 })
 
@@ -59,7 +65,7 @@ $('.video__close').click(function() {
 //datepicker
 $('.datepicker span').click(function() {
 	$('.datepicker span').removeClass('active');
-	$(this).addClass('active');
+	$(this).addClass('active');  
 });
 
 //order types
@@ -67,5 +73,66 @@ $('.order__type li').click(function() {
 	$('.order__type li').removeClass('active');
 	$(this).addClass('active');
 });
+
+//validator form
+$('.form-for-order').validate({
+  rules : {
+    name: {required : true, minlength: 2},
+    surname: {required : true, /*number: true,*/ minlength: 2},
+    patronymic: {required : true, minlength: 2},
+    comment: {required : true},
+    email: {required : true, email: true},
+    email1: {required : true, email: true},
+    fio: {required : true},
+    skype: {required : true},
+    phone: {required : true}
+  },
+  messages: {
+    name: {
+      required: "Введите ваше имя!",
+      minlength: "Введите не менее, чем 2 символа!"
+    },
+    surname: {
+      required: "Введите вашу фамилию!",
+      //number: "Фамилия недолжна содержать цифр!",
+      minlength: "Введите не менее, чем 2 символа!"
+    },
+    patronymic: {
+      required: "Введите ваше отчество!",
+      minlength: "Введите не менее, чем 2 символа!"
+    },
+    comment: {
+      required: "Введите комментарий!"
+    },
+    email: {
+      required: "Вы неуказали email для отправки!",
+      email: "Введите правильный email адрес!"
+    },
+    email1: {
+      required: "Вы неуказали ваш email!",
+      email: "Введите правильный email адрес!"
+    },
+    fio: {
+      required: "Вы неуказали ваше ФИО!"
+    },
+    skype: {
+      required: "Вы неуказали ваш skype!"
+    },
+    phone: {
+      required: "Введите 10 значный номер телефона!"
+    }
+  },
+  invalidHandler: function() {
+    $('.form-field input').each(function() {
+      if ($(this).hasClass('error')) {
+        var order_liefd = $(this).parent().attr('id');
+        $(window).scrollTo('#' + order_liefd, 800);
+        return false;
+      };      
+    });    
+  }
+});
+//phone mask
+$('.form-field_phone').mask("(999) 999-99-99");
 
 });
